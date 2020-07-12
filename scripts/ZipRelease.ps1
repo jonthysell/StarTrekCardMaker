3795f5ca22c9999cf4a0720536bfef63a4309773
Jon Thysell <thysell@gmail.com>
2020-07-11 22:22:08 -0700

    move 7z into build script

--- scripts/ZipRelease.ps1
+++ scripts/ZipRelease.ps1
@@ -18,6 +18,13 @@ Write-Host "Create package..."
 
 Set-Location -Path "$OutputRoot"
 
-7za a "$TargetOutputPackageName" "$TargetOutputDirectory" -tzip
+if (-Not (Test-Path ".\7za.exe")) {
+    Write-Host "Get 7zip"
+    curl https://www.7-zip.org/a/7zr.exe -o 7zr.exe 
+    curl https://www.7-zip.org/a/7z1900-extra.7z -o 7z1900-extra.7z
+.\7zr.exe e 7z1900-extra.7z -y 7za.exe
+}
+
+.\7za.exe a "$TargetOutputPackageName" "$TargetOutputDirectory" -tzip
 
 Set-Location -Path "$StartingLocation"
